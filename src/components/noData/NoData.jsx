a77d93bdd66ea4cece913554a4216c12473721ce
Adaikkalam <adaikkalarajaa@gmail.com>
2026-04-13 17:56:24 +0530

    image add

--- src/components/noData/NoData.jsx
+++ src/components/noData/NoData.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import "./noData.css";
-
+import noDataImage from "../../assets/noData.png";
 const NoData = () => {
   return (
     <div className="no-data-wrapper text-center ">
       <div className="no-data-box">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/6134/6134065.png"
-          alt="no-data"
-          className="no-data-img"
-        />
+        <img src={noDataImage} alt="no-data" className="no-data-img" />
 
         <h4 className="fw-bold mt-3">No Products Found</h4>
 
