e810adfa66317841fc93d425271e35e89a3bc315
adaikkalam-1 <adaikkalarajaa@gmail.com>
2026-04-14 09:48:56 -0700

    issue fixed egov not showing

--- src/components/eGovCard/EGovCard.jsx
+++ src/components/eGovCard/EGovCard.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const EGovCard = ({item}) => {
+const EGovCard = ({items}) => {
   return (
     <div className="latest-news-card">
       <div className="img-container">
         <img
-          src={item.image}
+          src={items?.image}
           alt="News 1"
+
         />
       </div>
       <div className="card-body">
-        <span className="category-tag">{item.name}</span>
-        <h5 className="card-title">{item.description}</h5>
+        <span className="category-tag">{items?.name}</span>
+        <h5 className="card-title">{items?.description}</h5>
         <Link to="/" className="btn-more">
           Read More <i className="fa-solid fa-arrow-right"></i>
         </Link>
